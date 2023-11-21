db7a15db22f2f801e78b012353924ee1706e580c
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-11-21 10:53:53 +0800

    routes created for above

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :items
+
+  resources :carts, except: [:new, :edit] do
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+  end
+
 end
