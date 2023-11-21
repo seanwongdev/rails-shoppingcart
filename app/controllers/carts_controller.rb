329e4bac68bee4458d36849c0647b9ba77f8ceee
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-11-21 17:24:29 +0800

    added cost functionality

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,5 +2,6 @@ class CartsController < ApplicationController
   def show
     @cart = Cart.find(params[:id])
     @cart_items = @cart.cart_items
+    @total_cost = @cart_items.sum { |cart_item| cart_item.quantity * cart_item.item.price.to_f }.round(2)
   end
 end
