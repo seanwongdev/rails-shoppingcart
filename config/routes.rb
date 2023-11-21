920983694729ee005cebf488279232d7a7d9c3de
sean wong <sean.wong.2009@business.smu.edu.sg>
2023-11-21 19:21:37 +0800

    fixed admin functionality for new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
-  resources :items
+  namespace :admin do
+    resources :items, only: %i[new create edit update destroy]
+  end
+
+  resources :items, only: %i[index show]
 
   resources :carts, except: %i[index new edit] do
     resources :cart_items, only: %i[index create update destroy]
